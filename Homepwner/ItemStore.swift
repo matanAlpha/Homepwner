4e4e752eb6c45b2f7348fc43e137fb9328d054bc
admin <admin@admins-imac.elcam.co.il>
2017-02-26 06:08:00 +0200

    With insets

---
+++ Homepwner/ItemStore.swift
@@ -0,0 +1,26 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by admin on 30 Shevat 5777.
+//  Copyright © 5777 Matan alpha. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        allItems.append(newItem)
+        return newItem
+    }
+}
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -15,6 +15,12 @@ class ItemsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Get the height of the status bar
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
     }
 
     override func didReceiveMemoryWarning() {
