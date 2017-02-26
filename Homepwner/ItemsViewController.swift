6cca88fc18995814b7f10c55a254f283a55b88d4
Admin <admin@macbook-air.local>
2017-02-26 09:12:55 +0200

    Do the silver tsk

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -30,7 +30,7 @@ class ItemsViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int {
-        return itemStore.allItems.count
+        return itemStore.allItems.count + 1
     }
 
     override func tableView(_ tableView: UITableView,
@@ -44,9 +44,18 @@ class ItemsViewController: UITableViewController {
         // Set the text on the cell with the description of the item
         // that is at the nth index of items, where n = row this cell
         // will appear in on the tableview
-        let item = itemStore.allItems[indexPath.row]
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        if (itemStore.allItems.count == indexPath.row) {
+            cell.textLabel?.text = "No more items!"
+            cell.detailTextLabel?.text = ""
+        }else{
+           let  item = itemStore.allItems[indexPath.row]
+            cell.textLabel?.text = item.name
+            cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        }
+        
+        
+       
         return cell
     }
 }
