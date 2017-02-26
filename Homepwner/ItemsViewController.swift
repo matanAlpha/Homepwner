f7be9223fb7aea26e07eac6fca3dc38fe7104450
Admin <admin@macbook-air.local>
2017-02-26 10:42:42 +0200

    with add and delete button

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -28,6 +28,36 @@ class ItemsViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func addNewItem(_ sender: UIButton) {
+        // Make a new index path for the 0th section, last row
+        // Create a new item and add it to the store
+        let newItem = itemStore.createItem()
+        // Figure out where that item is in the array
+        if let index = itemStore.allItems.index(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            // Insert this new row into the table
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    
+    }
+    
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+        // If you are currently in editing mode...
+        if isEditing {
+            // Change text of button to inform user of state
+            sender.setTitle("Edit", for: .normal)
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        } else {
+            // Change text of button to inform user of state
+            sender.setTitle("Done", for: .normal)
+            // Enter editing mode
+            setEditing(true, animated: true)
+        }
+    }
+    
+    
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
